7cb0282407e62310a7e2a7e0a52e9eb8a007e4c0
Joseph Pendon <pendon.josephjr@gmail.com>
2026-03-27 23:36:46 +0800

    feat: set up backend

---
+++ backend/src/config/database.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect
+        (`${process.env.MONGODB_URI}`)
+        console.log(`MongoDB connected successfully
+            Host: ${connectionInstance.connection.host}`);
+
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1); // Exit the process with an error code
+    }
+}
+
+export default connectDB;
