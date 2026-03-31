42b71a076a8fb4dadafd0a2a5403dbd211ad15bc
Joseph Pendon <pendon.josephjr@gmail.com>
2026-03-31 22:00:59 +0800

    feat: completed crud operation endpoints for reservations

--- backend/src/controllers/reservation.controllers.js
+++ backend/src/controllers/reservation.controllers.js
@@ -25,4 +25,45 @@ const createReservation = async (req, res) => {
     }
 }
 
-export { createReservation };
+const getReservations = async (req, res) => {
+    try {
+        const reservations = await Reservation.find();
+        res.status(200).json({message: "Reservations retrieved successfully", reservations});
+    } catch (error) {
+        res.status(500).json({message: "Error retrieving reservations", error});
+    }
+}
+
+
+const updateReservation = async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const updatedReservation = await Reservation.findByIdAndUpdate(id, req.body, {returnDocument: "after", runValidators: true});
+        if (!updatedReservation) {
+            return res.status(404).json({message: "Reservation not found"});
+        }
+
+        res.status(200).json({message: "Reservation updated successfully", reservation: updatedReservation});
+
+    } catch (error) {
+        res.status(500).json({message: "Error updating reservation", error});
+    }
+}
+
+const deleteReservation = async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const deletedReservation = await Reservation.findByIdAndDelete(id);
+        if (!deletedReservation) {
+            return res.status(404).json({message: "Reservation not found"});
+        }
+
+        res.status(200).json({message: "Reservation deleted successfully", reservation: deletedReservation});
+    } catch (error) {
+        res.status(500).json({message: "Error deleting reservation", error});
+    }
+}
+
+export { createReservation, getReservations, updateReservation, deleteReservation };
--- backend/src/routes/reservation.route.js
+++ backend/src/routes/reservation.route.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
-import { createReservation } from "../controllers/reservation.controllers.js";
+import { createReservation, getReservations, updateReservation, deleteReservation } from "../controllers/reservation.controllers.js";
 
 const router = Router();
     router.post("/create", createReservation);
+    router.get("/all", getReservations);
+    router.patch("/update/:id", updateReservation);
+    router.delete("/delete/:id", deleteReservation);
 export default router;
